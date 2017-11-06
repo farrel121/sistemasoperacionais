2a2fd3d469fe0fbb718a9cf74e23141f497eca34
farrel121 <31021522+farrel121@users.noreply.github.com>
2017-11-06 14:30:43 -0300

    Create Impressora.java

---
+++ Impressora.java
@@ -0,0 +1,13 @@
+package contador;
+
+public class Impressora extends Armazem{
+       
+    public static void main(String[] args) {
+        
+        Armazem a1 = new Armazem();
+        a1.armazem(); 
+        System.out.println(armazenado);
+    
+    }
+    
+}
